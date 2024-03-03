6146871bea125899faa7bc7e438d328af8df1252
DannySvoboda <155640918+DannySvoboda@users.noreply.github.com>
2024-03-03 03:35:41 +0000

    Complete overhaul

--- Button_Audio_Player.py
+++ Button_Audio_Player.py
@@ -4,20 +4,19 @@ for Eric Ouelette in a robotics project.
 Code is intended to wait for button press, then play audio file."""
 
 #import modules
-import aiy.voicehat
-import aiy.audio
+from aiy.board import Board,Led
+import aiy.voice.audio
 
 def main():
-#establish button
-  button = aiy.voicehat.get_button()
-
 #wait for button press
   while True:
-    print("Press the button for music!")
-    button.wait_for_press()
-    print("Button pressed, playing music!")
-    aiy.audio.play_wave("music_files/Cyborg Ninja.wav")
-    print("You played music!")
-
+    with Board() as board:
+      print("Press the button for music!")
+      board.button.wait_for_press()
+      board.led.state = Led.ON
+      print("Button pressed, playing music!")
+      aiy.audio.play_wav("music_files/Cyborg Ninja.wav")
+      print("You played music!")
+  
 if __name__ == "__main__":
   main()
